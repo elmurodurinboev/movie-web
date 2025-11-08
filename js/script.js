2234d541ac9be6cd44b893f550cdcdc14b006e21
elmurodurinboev <elmurodorinboyev@gmail.com>
2025-11-08 15:38:25 +0500

    Finish the project

--- js/script.js
+++ js/script.js
@@ -1,35 +1,76 @@
-const ads = document.querySelectorAll(".promo__adv img"),
-  genre = document.querySelector(".promo__genre"),
-  promoBg = document.querySelector(".promo__bg"),
-  seriesList = document.querySelector(".promo__interactive-list");
+document.addEventListener("DOMContentLoaded", () => {
+  const ads = document.querySelectorAll(".promo__adv img"),
+    genre = document.querySelector(".promo__genre"),
+    promoBg = document.querySelector(".promo__bg"),
+    seriesList = document.querySelector(".promo__interactive-list"),
+    addForm = document.querySelector("form.add"),
+    input = document.querySelector(".adding__input"),
+    favirite = document.querySelector("[type='checkbox']");
 
-const seriesDb = {
-  series: [
-    "OMAR",
-    "The Final Legacy",
-    "ERTUGRUL",
-    "MAGNIFICENT CENTURY",
-    "GREAT SELJUKS: GUARDIANS...",
-  ],
-};
+  const seriesDb = {
+    series: [
+      "OMAR",
+      "The Final Legacy",
+      "ERTUGRUL",
+      "MAGNIFICENT CENTURY",
+      "GREAT SELJUKS: GUARDIANS...",
+    ],
+  };
 
-ads.forEach((item) => {
-  item.remove();
-});
+  addForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let newValue = input.value;
+    const isFavorite = favirite.checked; //true/false
 
-genre.textContent = "COMEDY";
+    if (newValue) {
+      // Qo'shish
+      if (newValue.length >= 18) {
+        newValue = `${newValue.slice(0, 18)}...`;
+      }
 
-promoBg.style.backgroundImage = "url(../img/2.jpg)";
+      if (isFavorite) {
+        console.log("Sevimli serial qo'shildi");
+      }
 
-console.dir(seriesList);
+      seriesDb.series.push(newValue.toUpperCase());
+      setList();
 
-seriesList.innerHTML = "";
+      event.target.reset();
+    }
+  });
 
-seriesDb.series.forEach((item, index) => {
-  seriesList.innerHTML += `
-  	<li class="promo__interactive-item">
+  ads.forEach((item) => {
+    item.remove();
+  });
+
+  genre.textContent = "COMEDY";
+
+  promoBg.style.backgroundImage = "url(../img/2.jpg)";
+
+  function setList() {
+    seriesList.innerHTML = "";
+    sort();
+
+    seriesDb.series.forEach((item, index) => {
+      seriesList.innerHTML += `
+    <li class="promo__interactive-item">
         ${index + 1} ${item}
         <div class="delete"></div>
-    </li>
-  `;
+    </li>`;
+    });
+
+    document.querySelectorAll(".delete").forEach((trashBtn, index) => {
+      trashBtn.addEventListener("click", () => {
+        // event.target.parentNode.remove();
+        seriesDb.series.splice(index, 1);
+        setList();
+      });
+    }); //[btn1, btn2]
+  }
+
+  function sort() {
+    seriesDb.series.sort();
+  }
+
+  setList();
 });
